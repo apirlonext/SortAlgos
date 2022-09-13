024ae63c5c5688cf53b6dc37843d793137d83215
apirlonext <apirlogeanu@outlook.com>
2022-09-13 08:14:40 +0300

    BubbleSort done

---
+++ src/main/java/ap/sortalgos/BubbleSort.java
@@ -0,0 +1,31 @@
+package ap.sortalgos;
+
+public class BubbleSort {
+
+    public static void BubbleSortImpl(){
+        int[] myArray = {10, -10, 20, -20, 30, -30, 40, -40};
+
+        for (int lastUnsortedIndex = myArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex --){
+            for(int i = 0; i < lastUnsortedIndex; i++){
+                if (myArray[i] > myArray[i+1]){
+                    swap(myArray, i, i+1);
+                }
+            }
+        }
+        for (int i = 0; i < myArray.length; i++){
+            System.out.println(myArray[i]);
+        }
+    }
+
+    public static void swap(int [] array, int i, int j){
+        if(i == j){
+            return;
+        }
+
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
+
---
+++ src/main/java/ap/sortalgos/Main.java
@@ -0,0 +1,7 @@
+package ap.sortalgos;
+
+public class Main {
+    public static void main(String[] args) {
+        BubbleSort.BubbleSortImpl();
+    }
+}
